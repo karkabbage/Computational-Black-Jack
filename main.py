398e1fe343df848587f975d55a8e0d29f6ae463f
karkabbage <125765698+karkabbage@users.noreply.github.com>
2023-03-25 17:19:08 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+"""CSC111 Winter 2023 Project Phase 2: Blackjack - A Computational Analysis
+
+Module Description
+===============================
+
+This Python module contains a blablabla
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of the 
+CSC111 instructional team at the University of Toronto St. George campus. 
+All forms of distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for this material,
+please consult the Canadian Copyright Act.
+
+This file is Copyright (c) 2023 Alessia Ruberto, Karyna Lim, Rachel Kim, Sasha Chugani. 
+
+"""
+
+
+
+
+
+if __name__ == '__main__':
+    import python_ta
+    
+    python_ta.check_all(config={
+        'extra-imports': [],  # the names (strs) of imported modules
+        'allowed-io': [],     # the names (strs) of functions that call print/open/input
+        'max-line-length': 120
+    })
